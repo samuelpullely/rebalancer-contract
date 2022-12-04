d863a10898d3f7e43684ff0f38b7997ba2ce6c6f
Samuel Pullely <samuelpullely@hotmail.com>
2022-12-04 13:18:50 +0100

    Update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,1 +1,3 @@
 export CARDANO_NODE_MAGIC=1
+export PATH="/Applications/Daedalus Mainnet.app/Contents/MacOS:$PATH"
+export CARDANO_NODE_SOCKET_PATH="$HOME/cardano/testnet/pre-production/db/node.socket"
